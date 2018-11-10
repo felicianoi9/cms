16ae21ae6d8afe2e593aba1ec63d69b579460875
Rogerio Feliciano <felicianoi9@gmail.com>
2018-11-10 21:26:05 -0200

    ajuste no layout do formulário da página de configurações

--- views/painel/config.php
+++ views/painel/config.php
@@ -1,21 +1,37 @@
 <h1>Configurações</h1>
 
 <form method="POST" enctype="multipart/form-data">
-	Titulo do Site:<br/>
-	<input type="text" name="site_title" value="<?php echo $site_title; ?>" /><br/><br/>	
-
-	Texto de boas vindas:<br/>
-	<input type="text" name="home_welcome" value="<?php echo $home_welcome; ?>" /><br/><br/>
-
-	Template do site:<br/>
-	<select name="site_template">
-		<option value="default" <?php echo ($site_template=='default')?'selected="selected"':''; ?>>Padrão</option>
-		<option value="default2" <?php echo ($site_template=='default2')?'selected="selected"':''; ?>>Padrão 2</option>
-		<option value="construcao" <?php echo ($site_template=='construcao')?'selected="selected"':''; ?>>Em Construção</option>
-	</select><br/><br/>
-
-	Banner da página inicial:<br/>
-	<input type="file" name="home_banner" /><br/><br/>
-
+    <fieldset>
+        <legend>
+            
+                Titulo do Site:
+            
+        </legend>
+	    <input type="text" name="site_title" value="<?php echo $site_title; ?>" />	
+    </fieldset>
+    <fieldset>
+        <legend>
+            
+                Texto de boas vindas:
+            
+        </legend>    
+	    <input type="text" name="home_welcome" value="<?php echo $home_welcome; ?>" />
+    </fieldset>
+    <fieldset>
+        <legend>            
+                Template do site:            
+        </legend>
+        <select name="site_template">
+            <option value="default" <?php echo ($site_template=='default')?'selected="selected"':''; ?>>Padrão</option>
+            <option value="default2" <?php echo ($site_template=='default2')?'selected="selected"':''; ?>>Padrão 2</option>
+            <option value="construcao" <?php echo ($site_template=='construcao')?'selected="selected"':''; ?>>Em Construção</option>
+        </select>
+    </fieldset>
+    <fieldset>
+        <legend >            
+            Banner da página inicial:           
+        </legend>
+	<input type="file" name="home_banner" />
+    </fieldset>
 	<input type="submit" class="botao add" value="Salvar" />
 </form>
